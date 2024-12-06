b9e88ec36f5d9212a661e644fa5ffcebb6328f47
benmuratseker <benmuratseker@hotmail.com>
2024-12-06 23:08:37 +0100

    service test

--- src/TennisBookings.Merchandise.Api/Controllers/StockController.cs
+++ src/TennisBookings.Merchandise.Api/Controllers/StockController.cs
@@ -1,5 +1,8 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TennisBookings.Merchandise.Api.Data;
 using TennisBookings.Merchandise.Api.Models.Output;
+using TennisBookings.Merchandise.Api.Stock;
 
 namespace TennisBookings.Merchandise.Api.Controllers;
 
@@ -7,11 +10,22 @@ namespace TennisBookings.Merchandise.Api.Controllers;
 [Route("api/[controller]")]
 public class StockController : ControllerBase
 {
+    private readonly IProductDataRepository _productDataRepository;
+    private readonly IStockCalculator _stockCalculator;
+
+    public StockController(IProductDataRepository productDataRepository, IStockCalculator stockCalculator)
+    {
+        _productDataRepository = productDataRepository;
+        _stockCalculator = stockCalculator;
+    }
     [HttpGet("total")]
-    public IActionResult GetStockTotal()
+    public async Task<ActionResult<StockTotalOutputModal>> GetStockTotal()
     {
         // return Ok("{\"stockItemTotal\":100}");
         // return new JsonResult("{\"stockItemTotal\":100}");
-        return Ok(new StockTotalOutputModal(){ StockItemTotal = 100 });
+        var products = await _productDataRepository.GetProductsAsync();
+        var stockTotalCount = _stockCalculator.CalculateStockTotal(products);
+        
+        return Ok(new StockTotalOutputModal(){ StockItemTotal = stockTotalCount });
     }
 }
--- test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/StockControllerTests.cs
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/StockControllerTests.cs
@@ -1,22 +1,30 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using TennisBookings.Merchandise.Api.Data.Dto;
+using TennisBookings.Merchandise.Api.External.Database;
+using TennisBookings.Merchandise.Api.IntegrationTests.Fakes;
 using TennisBookings.Merchandise.Api.IntegrationTests.Models;
 
 namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers;
 
-public class StockControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+public class
+    StockControllerTests : IClassFixture<WebApplicationFactory<Startup>>
 {
     private readonly HttpClient _client;
+    private readonly WebApplicationFactory<Startup> _factory;
 
     public StockControllerTests(WebApplicationFactory<Startup> factory)
     {
-        _client = factory.CreateDefaultClient(new Uri("http://localhost/api/stock/") );
+        factory.ClientOptions.BaseAddress = new Uri("http://localhost/api/stock/");
+        _client = factory.CreateClient();
+        _factory = factory;
     }
 
     [Fact]
     public async Task GetStockTotal_ReturnsSuccessStatusCode()
     {
         var response = await _client.GetAsync("total");
-        
+
         response.EnsureSuccessStatusCode();
     }
 
@@ -24,23 +32,24 @@ public class StockControllerTests : IClassFixture<WebApplicationFactory<Startup>
     public async Task GetAll_ReturnsExpectedJsonContentString()
     {
         var response = await _client.GetStringAsync("total");
-        
-        Assert.Equal("{\"stockItemTotal\":100}",response);
+
+        Assert.Equal("{\"stockItemTotal\":100}", response);
     }
 
     [Fact]
     public async Task GetAll_ReturnsExpectedJsonContentType()
     {
         var response = await _client.GetAsync("total");
-        
-        Assert.Equal("application/json", response?.Content?.Headers?.ContentType?.MediaType);
+
+        Assert.Equal("application/json",
+            response?.Content?.Headers?.ContentType?.MediaType);
     }
 
     [Fact]
     public async Task Get_ReturnsExpectedJson()
     {
         var model = await _client.GetFromJsonAsync<ExpectedStockTotalOutputModel>("total");
-
+        
         Assert.NotNull(model);
         Assert.True(model.StockItemTotal > 0);
     }
@@ -48,8 +57,24 @@ public class StockControllerTests : IClassFixture<WebApplicationFactory<Startup>
     [Fact]
     public async Task Get_ReturnsExpectedStockQuantity()
     {
-        var model = await _client.GetFromJsonAsync<ExpectedStockTotalOutputModel>("total");
+        var cloudDatabase = new FakeCloudDatabase(new[]
+        {
+            new ProductDto { StockCount = 200 },
+            new ProductDto { StockCount = 500 },
+            new ProductDto { StockCount = 300 }
+        });
+
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<ICloudDatabase>(cloudDatabase);
+            });
+        }).CreateClient();
         
+        var model = await _client.GetFromJsonAsync<ExpectedStockTotalOutputModel>(
+                "total");
+
         Assert.Equal(1000, model?.StockItemTotal);
     }
 }
